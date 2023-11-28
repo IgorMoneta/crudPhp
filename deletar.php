931710808e0bfa881fd82109ca0a6175d83e23df
IgorMoneta <130601630+IgorMoneta@users.noreply.github.com>
2023-11-28 15:25:10 -0300

    deletar.php

--- deletar.php
+++ deletar.php
@@ -17,24 +17,12 @@ $sql = "DELETE FROM `bebidas` WHERE id = $id";
 $dados = mysqli_query($conn , $sql);
 
 ?>
-    <h1>Deletar Bebida</h1>
+    <h1>Bebida deletada</h1>
     <br>
-    <h3>Confirme se deseja deletar este produto</h3>
-    <form action="excluir_cadastro.php" method="POST">
-        <p>
-            <input type="hidden" name="nome" required value="<?php echo $linha ['nome']?>">
-       
-            <input type="hidden" name="quantidade" required value="<?php echo $linha ['quantidade']?>">
-       
-            <input type="hidden" name="estoque" required value="<?php echo $linha ['estoque']?>">
-            
-            <input type="hidden" name="data" required value="<?php echo $linha ['data']?>">
-        </p>
-        
-            <section>
-                <input id="excluir" type="submit" value="EXCLUIR">
-                <input type = "hidden" name = "id" value = "<?php echo $linha ['id']?>">
-            </section>
-    </form>
+    <h2>Volte para o local que deseja, clicando abaixo:</h2>
+        <section>
+        <a  href="../pesquisa.php">Pesquisa</a>
+        <a  href="../cadastros/index.php">Cadastro</a>
+        </section>
 </body>
 </html>
